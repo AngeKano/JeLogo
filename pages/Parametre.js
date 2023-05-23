d2269f017935187d4c6adfa80d34e7f5a821d1c2
AngeKano <Angekano002@gmail.com>
2023-05-23 23:47:28 +0000

    owner change Parametre

--- pages/Parametre.js
+++ pages/Parametre.js
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Parametre = () => {
   return (
     <View>
       <Text>Parametre</Text>
+      <Pressable>
+        <Text>Change your password</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default Parametre
+export default Parametre;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
