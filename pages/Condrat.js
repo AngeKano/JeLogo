b9468ee0e70f2a709d19bf7c5f2ed3fe25177dfe
AngeKano <Angekano002@gmail.com>
2023-09-27 23:12:32 +0000

    VV 27/9/23

---
+++ pages/Condrat.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Condrat = () => {
+  return (
+    <View>
+      <Text>Condrat</Text>
+    </View>
+  );
+};
+
+export default Condrat;
